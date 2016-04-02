94a3e2db892077bc01061700f8e89a4e0e98a4e4
aljumaro <aljumaro@gmail.com>
2016-04-02 21:26:55 +0200

    External properties file loaded by JVM argument

--- src/main/java/com/aljumaro/techtest/Application.java
+++ src/main/java/com/aljumaro/techtest/Application.java
@@ -3,6 +3,10 @@ package com.aljumaro.techtest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySources;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 /**
  * Application class
@@ -15,4 +19,5 @@ public class Application {
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
     }
+
 }
